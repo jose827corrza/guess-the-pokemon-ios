b8ee41446afbe92a83ab75d1176f201b45a16df2
joseDev <jose96corrza@gmail.com>
2023-10-10 10:15:23 -0500

    Views created, commit before implement MVC arch

--- GuestThePokemon/ViewController.swift
+++ GuestThePokemon/ViewController.swift
@@ -9,11 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var pokemonImage: UIImageView!
+    @IBOutlet weak var labelScore: UILabel!
+    @IBOutlet weak var labelMessage: UILabel!
+    @IBOutlet var answerButtons: [UIButton]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        //Can set a shawod to the button, using the colors from each pokemon
     }
 
-
+    @IBAction func buttonPressed(_ sender: UIButton) {
+    }
+    
 }
 
